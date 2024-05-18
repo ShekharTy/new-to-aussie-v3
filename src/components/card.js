744e58e905901f8107d3a0bde8d6711afa67b64f
Mirudhula <mraj0010@student.monash.edu>
2024-05-18 22:39:38 +1000

    final updates done on restaurant

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import restaurantDetails from './restaurant';
+import "../styles/card.css";
 
 function CardComponent({ title, description, image, restaurantDetails, toggleDetails, webUrl}) {
   const [showDetails, setShowDetails] = useState(false);
@@ -11,16 +12,15 @@ function CardComponent({ title, description, image, restaurantDetails, toggleDet
   };
 
   return (
-    <a className="relative block bg-white border shadow-sm rounded-md dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 hover:bg-zinc-800 transition duration-300" href={webUrl} target="_blank" rel="noopener noreferrer" onClick={toggleCardDetails}>
+    <a className="relative block bg-white border shadow-sm rounded-md dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 hover:bg-zinc-800 transition duration-300 card-link" href={webUrl} target="_blank" rel="noopener noreferrer" onClick={toggleCardDetails}>
       <div className="aspect-w-16 aspect-h-9">
         <img className="object-cover" src={image} alt="Image Description" />
       </div>
-     
       <div className="p-4 md:p-5">
-        <h3 className="text-3xl font-bold text-gray-800 dark:text-white">
+        <h3 className="text-3xl font-bold text-gray-500">
           {title}
         </h3>
-        <p className="mt-1 text-gray-500 dark:text-neutral-400">
+        <p className="mt-1 text-gray-500">
           {description}
         </p>
         <button className="mt-2 text-sm text-gray-500 dark:text-neutral-400 underline">
@@ -30,11 +30,10 @@ function CardComponent({ title, description, image, restaurantDetails, toggleDet
           <div className="mt-4 text-sm text-gray-500 dark:text-neutral-400">
             {restaurantDetails}
           </div>
-          
         )}
       </div>
     </a>
-  );
+  );  
 }
 
 export default CardComponent;
--- src/components/spin-wheel.js
+++ src/components/spin-wheel.js
@@ -1,21 +1,22 @@
-import spinWheelData from "../data/spin_wheel_data";
-import { useState } from "react";
+import React, { useState } from "react";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
 import "../styles/spin-wheel.css";
+import spinWheelData from "../data/spin_wheel_data";
 
 function createCasinoColorGenerator() {
   const casinoColors = [
-      "#ff0f7b", // Vivid Red
-      "#ff930f", // Black
-      "#45caff",
-      "red",
-      "blue",
+    "#ff0f7b", // Vivid Red
+    "#ff930f", // Black
+    "#45caff",
+    "red",
+    "blue",
   ];
   let currentIndex = 0;
 
   return function() {
-      const color = casinoColors[currentIndex];
-      currentIndex = (currentIndex + 1) % casinoColors.length;
-      return color;
+    const color = casinoColors[currentIndex];
+    currentIndex = (currentIndex + 1) % casinoColors.length;
+    return color;
   };
 }
 
@@ -23,7 +24,7 @@ function createCasinoColorGenerator() {
 const getCasinoColor = createCasinoColorGenerator();
 
 const cuisines = [];
-spinWheelData.map((current) => {
+spinWheelData.forEach((current) => {
   if (!cuisines.some((val) => current["Spin Cuisines"] === val.cuisine)) {
     cuisines.push({
       cuisine: current["Spin Cuisines"],
@@ -36,9 +37,12 @@ const SpinWheel = () => {
   const [initState, setInitState] = useState(true);
   const [randIndex, setRandIndex] = useState(0);
   const [showList, setShowList] = useState(false);
+
   return (
     <>
-      <h2 className="mt-3 text-center">Still haven't decided what to eat? Use the spinwheel to decide.</h2>
+      <h2 className="mt-3 text-center text-black">
+        Still haven't decided what to eat? Use the spinwheel to decide.
+      </h2>
       <div className="spin-container">
         {initState ? (
           <div
@@ -89,30 +93,60 @@ const SpinWheel = () => {
           ))}
         </div>
       </div>
-      {(!initState && showList) && (
-          <>
-          <h2 className="mx-3 mb-5 text-center">We Recommend: {cuisines[randIndex].cuisine}</h2>
-          <div className="grid grid-cols-4 gap-4 animate__animated animate__fadeInUp m-3 mb-5">
-          {spinWheelData
-            .filter((item) => item["Spin Cuisines"] === cuisines[randIndex].cuisine)
-            .map((cuisine) => {
-              return (
-                <div key={`${cuisine["Block ID"]}-${cuisine["Property ID"]}-${cuisine["Trading name"]}`} className="border p-4 bg-white rounded">
-                  <h3 className="text-lg font-semibold">{cuisine["Trading name"]}</h3>
-                  <p className="text-gray-600">{cuisine.Suburb}</p>
-                  <a
-                    href={cuisine["Google Search Link"]}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-600 underline hover:text-blue-800"
+      {!initState && showList && (
+        <>
+          <div className="container mx-auto relative">
+            <h2 className="mx-3 mb-5 text-center">
+              We Recommend: {cuisines[randIndex].cuisine}
+            </h2>
+            <Splide
+              options={{
+                perPage: 4,
+                gap: "1rem",
+                autoplay: true,
+                pauseOnHover: true,
+                pagination: false,
+                arrows: true,
+                type: "loop",
+                classes: {
+                  arrows: "splide__arrows",
+                  arrow: "splide__arrow",
+                  prev: "splide__arrow--prev",
+                  next: "splide__arrow--next",
+                },
+              }}
+            >
+              {spinWheelData
+                .filter((item) => item["Spin Cuisines"] === cuisines[randIndex].cuisine)
+                .map((cuisine) => (
+                  <SplideSlide
+                    key={`${cuisine["Block ID"]}-${cuisine["Property ID"]}-${cuisine["Trading name"]}`}
                   >
-                    Google Link
-                  </a>
-                </div>
-              );
-            })}
+                    <div className="border p-4 bg-white rounded">
+                      <div className="bg-white rounded-lg shadow-lg transition duration-300 transform hover:shadow-xl hover:scale-105">
+                        <div className="p-4">
+                          <h3 className="text-lg font-semibold">
+                            {cuisine["Trading name"]}
+                          </h3>
+                          <p className="text-gray-600">{cuisine.Suburb}</p>
+                          <a
+                            href={cuisine["Google Search Link"]}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-blue-600 underline hover:text-blue-800"
+                          >
+                            Google Link
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </SplideSlide>
+                ))}
+            </Splide>
           </div>
-          </>
+          <div></div>
+          <div></div>
+        </>
       )}
     </>
   );
